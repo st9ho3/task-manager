166df2c6afabeb3d4600ca0ab05ba8d15eabd732
st9ho3 <stahos21@gmail.com>
2024-10-30 21:02:04 +0200

    face profiles

--- src/Components/Layout/Main/TaskBoard/TaskList/TaskItem/TaskItemBot.jsx
+++ src/Components/Layout/Main/TaskBoard/TaskList/TaskItem/TaskItemBot.jsx
@@ -9,7 +9,7 @@ const TaskItemBot = () => {
   const taskIds = taskState.tasks.assignees
 
   const [x, setX] = useState([])
-//Working on the faces. Fetching the users
+//Working on the faces. Fetching the user
   /* useEffect(() => {
     if (taskState.tasks.length > 0) {
       const getUsers = () => {
